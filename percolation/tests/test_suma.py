6bf8f1aec75552b6abde5ab794aeae7fc8c9ddbb
Diego Prada <prada.gracia@gmail.com>
2023-06-02 17:22:44 -0600

    Prueba de test erroneo

---
+++ percolation/tests/test_suma.py
@@ -0,0 +1,12 @@
+"""
+Unit and regression test for the molsysmt package.
+"""
+
+# Import package, test suite, and other packages as needed
+import percolation
+import pytest
+
+def test_suma():
+    a = 3+6
+    assert a==10
+
